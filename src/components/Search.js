be914897da64b8489f1acd360cb61bd6a31859c3
KarinaLei <karinaleiii1223@gmail.com>
2021-01-15 16:31:44 -0800

    styled banner opacity

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,12 +21,14 @@ class Search extends React.Component {
         }
 
         this.bannerCss = {
-            color: '#42474c', 
+            // color: '#42474c', 
+            color: 'black',
             backgroundColor: 'white', 
             position: 'fixed', 
             top: '0',
             width: '100%',
-            height: '32px'
+            height: '32px',
+            opacity: 0.8
         }
     }
 
